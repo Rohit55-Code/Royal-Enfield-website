97719331948a7d2c02f30da1b6ce920869ee0be6
Rohit Patil <rohitp7387@gmail.com>
2025-09-28 23:47:48 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,27 @@
+function SuperMeteor(){
+    document.getElementById("Typesimg").src="./image/super.avif";
+}
+function Hunter350(){
+    document.getElementById("Typesimg").src="./image/hunter-350-right-front-three-quarter.webp";
+}
+function Scran411(){
+    document.getElementById("Typesimg").src="./image/scram.avif";
+}
+function Classic(){
+    document.getElementById("Typesimg").src="./image/classic.jpeg";
+}
+function Interceptor(){
+    document.getElementById("Typesimg").src="./image/interceptor.avif";
+}
+function ontinentalGt(){
+    document.getElementById("Typesimg").src="./image/favicon-32x32.png";
+}
+function meteor(){
+    document.getElementById("Typesimg").src="./image/royal-enfield-himalayan-modified-by-bulleteer-cust-5abb.jpg";
+}
+function SuperMeteor(){
+    document.getElementById("Typesimg").src="./image/super.avif";
+}
+function SuperMeteor(){
+    document.getElementById("Typesimg").src="./image/super.avif";
+}
